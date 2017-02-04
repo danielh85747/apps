3fa1b7dad8608d4e9d50af0f216bdbbbe9cc199f
apple user <appleuser@iMac-de-apple.local>
2017-02-04 12:30:30 -0500

    Color extensions added

--- Apps/ViewController.swift
+++ Apps/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("extension added!")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +22,13 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
 }
 
